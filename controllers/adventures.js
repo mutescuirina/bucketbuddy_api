4cea0763e68152d1842ea36eef4fea10e0fff3f1
Sara Coren <saracoren@Saras-MacBook-Air.local>
2019-05-10 18:01:23 -0400

    added seed data

--- controllers/adventures.js
+++ controllers/adventures.js
@@ -4,6 +4,15 @@ const adventures = express.Router()
 const Adventure = require('../models/adventuresmodels.js')
 //...farther down the page
 
+//Seed Route
+const newAdventure = require('../models/seed.js')
+adventures.get('/seed', (req, res) => {
+  Adventure.create(newAdventure, (err, seedItems)=> {
+       res.send(seedItems)
+   })
+ })
+
+
 //Index Route
 adventures.get('/', (req, res) => {
     Adventure.find({}, (err, foundAdventures) => {
