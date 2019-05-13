63271510f09a645f31b5c366be8dd53e0621da4b
acsirina <irina.a@irinas-MacBook-Pro.local>
2019-05-13 15:02:26 -0400

    finished heroku connection

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const PORT = process.env.PORT || 3003;
 const mongoose = require('mongoose')
 const MONGODB_URI =  process.env.MONGODB_URI || 'mongodb://localhost:27017'+ '/adventures'
 
+const whitelist = ['http://localhost:3001', 'https://tranquil-reef-57665.herokuapp.com/']
+const corsOptions = {
+  origin (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 // middleware
 app.use(express.json()); //use .json(), not .urlencoded()
 
@@ -23,8 +33,8 @@ app.get('/' , (req, res) => {
   });
 
   // Controllers/Routes
-  const cors = require('cors')
-  app.use(cors())
+  // const cors = require('cors')
+  // app.use(cors())// that is to allow for everyone to hack but useful for debugging
 
 const adventuresController = require('./controllers/adventures.js')
 app.use('/adventures', adventuresController)
