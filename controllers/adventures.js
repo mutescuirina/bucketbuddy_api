e33527410d6db78e5acd6632732ccd0137d5edb7
Alexandra Romanova <alexandraromanova@Alexandras-MacBook-Air.local>
2019-05-13 09:39:25 -0400

    corsadded

--- controllers/adventures.js
+++ controllers/adventures.js
@@ -5,13 +5,12 @@ const Adventure = require('../models/adventuresmodels.js')
 //...farther down the page
 
 //Seed Route
-const newAdventure = require('../models/seed.js')
-adventures.get('/seed', (req, res) => {
-  Adventure.create(newAdventure, (err, seedItems)=> {
-       res.send(seedItems)
-   })
- })
-
+// const newAdventure = require('../models/seed.js')
+// adventures.get('/seed', (req, res) => {
+//   Adventure.create(newAdventure, (err, seedItems)=> {
+//        res.send(seedItems)
+//    })
+//  })
 
 //Index Route
 adventures.get('/', (req, res) => {
